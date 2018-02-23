1ef55b0d07720b0ff2bc38a54b9e48902265ff17
thronbird <307365651@qq.com>
2018-02-23 17:13:38 +0800

    TestMultiStart

--- src/main/java/GC/MemorySpaceAndAllocation.java
+++ src/main/java/GC/MemorySpaceAndAllocation.java
@@ -4,29 +4,29 @@ public class MemorySpaceAndAllocation{
     public static void main(String[] args) throws Exception{
         Thread.sleep(2000);
         // should allocate from TLAB
-        MemoryObject object=new MemoryObject(new byte[1024]);
+        MemoryObjectx object=new MemoryObjectx(new byte[1024]);
         Thread.sleep(2000);
         // should allocate from Eden space,not TLAB
-        MemoryObject newObject=new MemoryObject(new byte[1024*1024*4]);
+        MemoryObjectx newObject=new MemoryObjectx(new byte[1024*1024*4]);
         Thread.sleep(2000);
         System.gc();
         Thread.sleep(2000);
-        object=new MemoryObject(new byte[1024]);
+        object=new MemoryObjectx(new byte[1024]);
         Thread.sleep(2000);
         // should allocate from Eden space,not TLAB
-        newObject=new MemoryObject(new byte[1024*1024*4]);
+        newObject=new MemoryObjectx(new byte[1024*1024*4]);
         Thread.sleep(2000);
         System.gc();
         Thread.sleep(2000);
     }
 }
 
-class MemoryObject{
+class MemoryObjectx{
     private byte[] bytes;
     public byte[] getBytes(){
         return bytes;
     }
-    public MemoryObject(byte[] bytes){
+    public MemoryObjectx(byte[] bytes){
         this.bytes=bytes;
     }
 }
