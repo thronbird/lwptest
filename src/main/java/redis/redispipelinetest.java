0cb5ff98f8c9ea0e7f152df42dd51cf445db083c
李万平 <20428@etransfar.com>
2018-08-16 11:16:54 +0800

    semaphore test

--- src/main/java/redis/redispipelinetest.java
+++ src/main/java/redis/redispipelinetest.java
@@ -1,5 +1,6 @@
 package redis;
 
+import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Pipeline;
 import redis.clients.jedis.Response;
@@ -39,7 +40,7 @@ public class redispipelinetest {
 		Jedis jedis = null;
 
 		try {
-			jedis = new Jedis("192.168.59.100", 6379);
+			jedis = new Jedis("127.0.0.1", 6379);
 			jedis.flushDB();
 			jedis.set("thekey", Integer.toString(0));
 
@@ -48,6 +49,20 @@ public class redispipelinetest {
 			}
 
 			System.out.println(jedis.get("thekey"));
+		} finally {
+			jedis.close();
+		}
+
+	}
+	@Test
+	public  void memoryTest() {
+		Jedis jedis = null;
+		try {
+			jedis = new Jedis("127.0.0.1", 6379);
+			jedis.flushDB();
+			for (int i = 1; i <= 10000; i++) {
+				jedis.lpush("bcScheduleOrderCache", "{Bill{billid=1113005515, transactiontype='充值', status='处理中', inputdate=Wed Aug 15 10:24:02 CST 2018, businessrecordnumber='1018081510243410002', businesstype='银联充值', updatedate=Wed Aug 15 14:11:06 CST 2018, refundtype='null', paymethod='bankPay'}");
+			}
 		} finally {
 			jedis.close();
 		}
